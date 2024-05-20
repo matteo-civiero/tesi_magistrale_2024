2c741e4ca63d49a20a1ad66ec1d3311358a798d1
matteo-civiero <matteo.civiero@studenti.unipd.it>
2024-05-20 10:20:34 +0200

    ottimizzazione dinamica ostacoli, anche se non è il problema delle prestazioni

--- MatlabCodes/obstacles_dynamics.m
+++ MatlabCodes/obstacles_dynamics.m
@@ -1,10 +1,9 @@
-function [new_obstacles] = obstacles_dynamics(obstacles, Ts)
+function [obstacles] = obstacles_dynamics(obstacles, Ts)
 %OBSTACLES_DYNAMICS function that moves the obstacles every iteration of
 %the MPC algorithm
 %   Every obstacle with a non-null velocity is moved in the correct
 %   location after every iteration
-new_obstacles = obstacles; % to copy the informations
 for i = 1:length(obstacles)
-    new_obstacles{i}.center = obstacles{i}.center + (obstacles{i}.velocity * Ts);
+    obstacles{i}.center = obstacles{i}.center + (obstacles{i}.velocity * Ts);
 end
 
