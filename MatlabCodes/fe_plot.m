fd13809643aa04eff6405389b7b1d3a3793dc978
matteo-civiero <matteo.civiero@studenti.unipd.it>
2024-11-26 15:22:01 +0100

    plotting

--- MatlabCodes/fe_plot.m
+++ MatlabCodes/fe_plot.m
@@ -1,11 +1,27 @@
 figure;
 hold on; grid on;
-plot(0:0.1:(0.1*(371-1)), form_err_15, "green", "LineWidth", 1.5);
-plot(0:0.1:(0.1*(202-1)), form_err_10, "blue", "LineWidth", 2);
-plot(0:0.1:(0.1*(208-1)), form_err_6, "red", "LineWidth", 3);
+plot(0:0.1:(0.1*(271-1)), form_err_15, "green", "LineWidth", 1.5);
+plot(0:0.1:(0.1*(466-1)), form_err_10, "blue", "LineWidth", 2);
+plot(0:0.1:(0.1*(315-1)), form_err_6, "red", "LineWidth", 3);
 %title("\textbf{Formation error comparison between different measurement noise simulations [m]}", "Interpreter","latex","FontSize", 14);
 set(gca, "FontSize", 20);
 xlim([0 20]);
 xlabel("Time [s]", "Interpreter","latex","FontSize", 40);
-ylabel("Formation error [m]", "Interpreter","latex","FontSize", 40);
+ylabel("\(|fe_{L,F}|\) [m]", "Interpreter","latex","FontSize", 40);
 legend("15cm", "10cm", "6cm", "Interpreter","latex","FontSize", 25);
+
+%%
+
+figure;
+hold on; grid on;
+plot(0:0.1:(0.1*(179-1)), form_err_2, "green", "LineWidth", 1.5);
+plot(0:0.1:(0.1*(150-1)), form_err_1, "blue", "LineWidth", 2);
+plot(0:0.1:(0.1*(151-1)), form_err_05, "red", "LineWidth", 3);
+%title("\textbf{Formation error comparison between different measurement noise simulations [m]}", "Interpreter","latex","FontSize", 14);
+set(gca, "FontSize", 20);
+xlim([0 14]);
+xlabel("Time [s]", "Interpreter","latex","FontSize", 40);
+ylabel("\(|fe_{L,F}|\) [m]", "Interpreter","latex","FontSize", 40);
+legend("2cm", "1cm", "5mm", "Interpreter","latex","FontSize", 25);
+
+%%
